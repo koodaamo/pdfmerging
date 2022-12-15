bc2b2cd5603908e91786ae3b9111227531fdb63c
Petri Savolainen <petri@koodaamo.fi>
2022-12-15 18:52:08 +0200

    do fields extraction in a separate process, split it into the producer module, fix routing overlapa

--- pdfmerging/producer.py
+++ pdfmerging/producer.py
@@ -1,9 +1,17 @@
 import sys
+import fitz
+import yaml
 from pathlib import Path
-from .merging import merge_file
+from .merging import merge_file, extract_fields
 
 
-def produce_pdf(source_pdf_path:Path, fields_dump_path:Path, merged_file_path:Path, data:dict):
+def produce_definitions(definitions_pdf_path:Path, fields_dump_path:Path):
+    with fitz.open(definitions_pdf_path) as defs:
+        fields = extract_fields(defs)
+        with open(fields_dump_path, "w", encoding="utf-8") as dumpfile:
+            dumpfile.write(yaml.dump(tuple(fields)))
+
+def produce_merged(source_pdf_path:Path, fields_dump_path:Path, merged_file_path:Path, data:dict):
     doc = merge_file(source_pdf_path, fields_dump_path, data)
     with open(merged_file_path, mode='wb') as out:
         out.write(doc.tobytes())
--- pdfmerging/server.py
+++ pdfmerging/server.py
@@ -1,5 +1,6 @@
 import asyncio
 import sys
+import logging
 from datetime import datetime
 from concurrent.futures import ProcessPoolExecutor
 from pathlib import Path
@@ -8,13 +9,18 @@ from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from fastapi import HTTPException, Depends
 from pydantic import BaseModel
 from fastapi.staticfiles import StaticFiles
-from .producer import produce_pdf
 from .config import merged_root, setup_root, generated_url_root
-from .config import templatefile, fieldsfile, host, token
+from .config import templatefile, fieldsfile, definitionsfile, host, token, loglevel, workers
+from .producer import produce_merged, produce_definitions
 
 
-num_jobs = 10
-executor = ProcessPoolExecutor(max_workers=num_jobs)
+logging.basicConfig()
+logger = logging.getLogger("pdfgen")
+logger.setLevel(loglevel)
+logger.info(f"loglevel set to {logger.level}")
+
+
+executor = ProcessPoolExecutor(max_workers=workers)
 
 security = HTTPBearer()
 
@@ -31,11 +37,10 @@ class MergedPDF(BaseModel):
 
 
 app = FastAPI(title='PDF Merger', version="1.0", openapi_tags=tag_defs)
-app.mount(f"/{generated_url_root}", StaticFiles(directory=merged_root), name="merged")
 
-@app.post("/{org_id}/{doc_id}", tags=["merging"], response_model=MergedPDF)
-async def merge(request:Request, org_id:str=Path(title="id of the organization"), doc_id:str=Path(title="id of the document case"), credentials=Depends(security)):
-    "Merge the given document with the submitted x-www-urlencoded form data"
+@app.post("/" + generated_url_root + "/{org_id}/{doc_id}", tags=["merging"], response_model=MergedPDF)
+async def generate_pdf(request:Request, org_id:str=Path(title="id of the organization"), doc_id:str=Path(title="id of the document case"), credentials=Depends(security)):
+    "Merge the given document with the submitted x-www-urlencoded form data into a new PDF and return a link to it."
 
     if credentials.credentials != token:
         raise HTTPException(status_code=403, detail="Incorrect token")
@@ -53,18 +58,35 @@ async def merge(request:Request, org_id:str=Path(title="id of the organization")
         raise HTTPException(status_code=404, detail=f"No such doc '{doc_id}' available")
     if not source_pdf_path.is_file():
         raise HTTPException(status_code=404, detail="template not available")
+
     if not fields_dump_path.is_file():
-        raise HTTPException(status_code=404, detail="field definitions not available")
+        definitions_pdf_path = doc_root / definitionsfile
+        if not definitions_pdf_path.is_file():
+            raise HTTPException(status_code=404, detail="neither fields config nor field definitions source PDF found")
+
+        extractor = loop.run_in_executor(executor, produce_definitions, definitions_pdf_path, fields_dump_path)
+        try:
+            await extractor
+        except Exception as exc:
+            logger.error(f"no fields config found and definitions extraction from {definitions_pdf_path} failed: ", exc)
+            raise HTTPException(status_code=500, detail="generation of fields config from definitions source PDF failed")
 
     outfilename = datetime.now().isoformat() + ".pdf"
     merged_file_path = merged_root / f"{org_id}/{doc_id}" / outfilename
+    merged_file_path.parent.mkdir(parents=True, exist_ok=True)
 
     loop = asyncio.get_running_loop()
-    merger = loop.run_in_executor(executor, produce_pdf, source_pdf_path, fields_dump_path, merged_file_path, dict(form))
+    merger = loop.run_in_executor(executor, produce_merged, source_pdf_path, fields_dump_path, merged_file_path, dict(form))
     try:
         await merger
     except Exception as exc:
-        raise HTTPException(status_code=500, detail=f"Merge failed: {exc}")
+        logger.error(f"PDF merge for {org_id}/{doc_id} failed: ", exc)
+        raise HTTPException(status_code=500, detail="PDF merge failed")
+    else:
+        logger.debug(f"merge operation successfully performed")
 
     return {"url": f"https://{host}/{generated_url_root}/{org_id}/{doc_id}/{outfilename}"}
 
+
+# The static route has to be after the merge endpoint, in this case
+app.mount(f"/{generated_url_root}", StaticFiles(directory=merged_root), name="merged")
